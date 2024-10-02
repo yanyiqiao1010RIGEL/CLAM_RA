b23c1b3ecce46cbc0cb05df525f6a57cc98db328
颜弋乔 <yanyiqiao@192-168-1-119.tpgi.com.au>
2024-10-03 02:12:07 +1000

    Path no need join h5_file

--- main.py
+++ main.py
@@ -183,7 +183,7 @@ elif args.task == 'task_2_tumor_subtyping':
 elif args.task == 'task_3_tgca':
     args.n_classes=2
     dataset = Generic_MIL_Dataset(csv_path = 'dataset_csv/TrainLabel1.csv',
-                            data_dir= os.path.join(args.data_root_dir, 'h5_files'),
+                            data_dir= args.data_root_dir,
                             label_col='IDH',
                             shuffle = False,
                             seed = args.seed,
