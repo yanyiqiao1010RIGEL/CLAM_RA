a2f7bb93724297004e069c072fac843d31ff9d88
颜弋乔 <yanyiqiao@192-168-1-119.tpgi.com.au>
2024-10-08 10:50:12 +1100

    ckpt map to cpu

--- utils/eval_utils.py
+++ utils/eval_utils.py
@@ -35,7 +35,7 @@ def initiate_model(args, ckpt_path, device='cuda'):
 
     print_network(model)
 
-    ckpt = torch.load(ckpt_path)
+    ckpt = torch.load(ckpt_path, map_location=torch.device('cpu'))
     ckpt_clean = {}
     for key in ckpt.keys():
         if 'instance_loss_fn' in key:
