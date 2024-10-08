38def2163c2e77f24c217bd4a81fc831ac1ff4c2
颜弋乔 <yanyiqiao@192-168-1-119.tpgi.com.au>
2024-10-08 11:09:54 +1100

    Test on cpu

--- utils/eval_utils.py
+++ utils/eval_utils.py
@@ -16,7 +16,7 @@ from sklearn.metrics import f1_score
 from sklearn.preprocessing import label_binarize
 import matplotlib.pyplot as plt
 
-def initiate_model(args, ckpt_path, device='cuda'):
+def initiate_model(args, ckpt_path, device='None'):
     print('Init Model')    
     model_dict = {"dropout": args.drop_out, 'n_classes': args.n_classes, "embed_dim": args.embed_dim}
     
@@ -38,7 +38,7 @@ def initiate_model(args, ckpt_path, device='cuda'):
     if device is None:
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    ckpt = torch.load(ckpt_path, map_location=device)
+    ckpt = torch.load(ckpt_path, map_location=torch.device('cpu'))
     ckpt_clean = {}
     for key in ckpt.keys():
         if 'instance_loss_fn' in key:
