95f6f378539fd847c24eb0679a9bd42f1c45887e
颜弋乔 <yanyiqiao@192-168-1-119.tpgi.com.au>
2024-10-09 10:16:48 +1100

    Modified keyerror in eval

--- eval.py
+++ eval.py
@@ -164,7 +164,7 @@ if __name__ == "__main__":
         model, patient_results, test_error, auc, df, f1 = eval(split_dataset, args, ckpt_paths[ckpt_idx])
         ### ensemble
         all_predictions.append(df['prediction'].values)
-        sample_ids = df['sample_id'].values
+        sample_ids = df['slide_id'].values
 
         all_results.append(all_results)
         all_auc.append(auc)
@@ -177,7 +177,7 @@ if __name__ == "__main__":
     majority_vote_predictions = np.apply_along_axis(lambda x: np.bincount(x).argmax(), axis=0, arr=all_predictions)
 
     # Save file
-    ensemble_df = pd.DataFrame({'sample_id': sample_ids, 'ensemble_prediction': majority_vote_predictions})
+    ensemble_df = pd.DataFrame({'slide_id': sample_ids, 'ensemble_prediction': majority_vote_predictions})
     ensemble_df.to_csv(os.path.join(args.save_dir, 'ensemble_predictions.csv'), index=False)
 
     final_df = pd.DataFrame({'folds': folds, 'test_auc': all_auc, 'test_acc': all_acc, 'test_f1': all_f1})
