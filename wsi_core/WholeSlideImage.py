95a5d8d8cfafb943352f87872a12c140fc9ffffb
颜弋乔 <yanyiqiao@192.168.10.4>
2024-12-06 10:19:45 +0800

    Change contours variables in WSI

--- wsi_core/WholeSlideImage.py
+++ wsi_core/WholeSlideImage.py
@@ -33,8 +33,10 @@ class WholeSlideImage(object):
         self.level_downsamples = self._assertLevelDownsamples()
         self.level_dim = self.wsi.level_dimensions
     
-        self.contours_tissue = None
-        self.contours_tumor = None
+        #self.contours_tissue = None
+        self.contours_tissue = []
+        #self.contours_tumor = None
+        self.contours_tumor = []
         self.hdf5_file = None
 
     def getOpenSlide(self):
