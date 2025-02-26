2397b4b4c54085d61c2c00966c73480cae0685d9
yanyiqiao <1826712823@qq.com>
2025-02-26 12:42:38 +1100

    Test

--- utils/utils.py
+++ utils/utils.py
@@ -65,6 +65,7 @@ def get_split_loader(split_dataset, training = False, testing = False, weighted 
 	if not testing:
 		if training:
 			if weighted:
+				print("Saaaaaaaaample from split_dataset:", split_dataset[0])
 				weights = make_weights_for_balanced_classes_split(split_dataset)
 				print("SSSSSSSSSample from split_dataset:", split_dataset[0])
 				loader = DataLoader(split_dataset, batch_size=1, sampler = WeightedRandomSampler(weights, len(weights)), collate_fn = collate_MIL, **kwargs)
