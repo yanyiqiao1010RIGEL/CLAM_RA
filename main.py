26a0e91a8e4962ff7119d8efb368fa638d589c84
yanyiqiao <1826712823@qq.com>
2025-03-16 16:34:31 +1100

    Update path

--- main.py
+++ main.py
@@ -206,7 +206,7 @@ elif args.task == 'task_3_tgca':
 ###Rigel add task 4
 elif args.task == 'task_4_hpa':
     args.n_classes=28##########
-    dataset = Generic_MIL_Dataset(csv_path = "dataset_csv/train_hpa_onehot.csv", #'dataset_csv/train_hpa_onehot.csv',
+    dataset = Generic_MIL_Dataset(csv_path = "/g/data/au38/CLAM/dataset_csv/train_hpa_onehot.csv", #'dataset_csv/train_hpa_onehot.csv',
                             data_dir= args.data_root_dir,
                             shuffle = False,
                             seed = args.seed,
