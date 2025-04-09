199fd92e7e03fe5b15244a5e31b4f0fb4a0a0a98
Yiqiao Yan <z5495200@ad.unsw.edu.au>
2025-04-09 11:49:57 +1000

    enhanced_multihot.csv

--- dataset_csv/multihot.py
+++ dataset_csv/multihot.py
@@ -2,11 +2,11 @@ import pandas as pd
 import numpy as np
 
 # 定义类别总数
-NUM_CLASSES = 28
+NUM_CLASSES = 27
 
 # 读取原始 CSV 文件
-input_csv = "train_hpa.csv"
-output_csv = "train_hpa_onehot.csv"
+input_csv = "enhanced.csv"
+output_csv = "enhanced_multihot.csv"
 df = pd.read_csv(input_csv)
 
 # 定义一个函数，将标签转化为多热编码
@@ -14,7 +14,7 @@ def multi_hot_encode(labels, num_classes):
     # 创建全零数组
     one_hot = np.zeros(num_classes, dtype=np.int32)
     # 将标签对应的索引置为 1
-    for label in map(int, labels.split(' ')):
+    for label in map(int, labels.split(';')):
         one_hot[label] = 1
     return one_hot
 
